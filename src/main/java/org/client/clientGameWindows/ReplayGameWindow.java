353d3542d7f1f63243cb48bba938db3c74cb2323
mplociniczak <marta.plociniczak09@gmail.com>
2024-01-30 17:29:44 +0100

    odtwarzanie ruchów z bazy wstecz

--- src/main/java/org/client/clientGameWindows/ReplayGameWindow.java
+++ src/main/java/org/client/clientGameWindows/ReplayGameWindow.java
@@ -91,7 +91,7 @@ public class ReplayGameWindow extends JFrame implements Runnable {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Previous");
-                //replayGameBackwards();
+                replayGameBackwards();
             }
         });
 
@@ -127,9 +127,58 @@ public class ReplayGameWindow extends JFrame implements Runnable {
 
     }
 
-    private void replayGameBackwards(){
-        //TODO: idk how to do it
-        //TODO: me neither XD
+    private void replayGameBackwards() {
+        if (currentMoveIndex > 0) {
+            // Cofnij indeks ruchu
+            currentMoveIndex--;
+
+            // Wyczyść planszę
+            clearGameBoard();
+
+            // Rysuj wszystkie ruchy do currentMoveIndex - 1
+            for (int i = 0; i < currentMoveIndex; i++) {
+                Move move = moves.get(i);
+                draw.updateStoneGraphics(move.getPositionX(), move.getPositionY(), move.getStoneColor(), gameBoardPanel);
+            }
+        }
+    }
+
+
+    private void clearGameBoard() {
+
+        // Usunięcie wszystkich obecnych komponentów z gameBoardPanel
+        gameBoardPanel.removeAll();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                JPanel square = new JPanel() {
+
+                    @Override
+                    protected void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+
+                        Graphics2D g2d = (Graphics2D) g;
+                        g2d.setColor(Color.BLACK);
+
+                        // Grubsza linia pionowa na środku kwadratu
+                        g2d.setStroke(new BasicStroke(2.0f));
+                        g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+
+                        // Grubsza linia pozioma na środku kwadratu
+                        g2d.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
+                    }
+                };
+
+                square.setBackground(Color.ORANGE);
+
+                square.setPreferredSize(new Dimension(25, 25));
+
+                gameBoardPanel.add(square);
+            }
+        }
+        // Odświeżanie gameBoardPanel, aby wyświetlić zmiany
+        gameBoardPanel.revalidate();
+        gameBoardPanel.repaint();
     }
 
     @Override
