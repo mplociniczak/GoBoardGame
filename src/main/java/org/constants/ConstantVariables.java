fa812be66fa912345aa5bcedd809ef06f904e239
mplociniczak <marta.plociniczak09@gmail.com>
2024-01-31 13:44:56 +0100

    finishing touches

--- src/main/java/org/constants/ConstantVariables.java
+++ src/main/java/org/constants/ConstantVariables.java
@@ -6,7 +6,7 @@ public final class ConstantVariables {
      * The port number on which the server listens for incoming connections.
      */
     public static int port = 6660;
-    public static String address = "192.168.7.218";
+    public static String address = "localhost";
     public static int errorCode = -1;
     public static int passCode = -2;
     public static int endgameCode = -3;
